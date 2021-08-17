e05bd7da2f5c9f9a5b2f6a10b848f00956ea115c
Avinashkumar190 <kumar.avi190@gmail.com>
2021-08-17 11:37:57 +0530

    Add files via upload

---
+++ MicroservicesEurekaServiceRegistryApplication.java
@@ -0,0 +1,14 @@
+package com.lara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@EnableEurekaServer
+public class MicroservicesEurekaServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesEurekaServiceRegistryApplication.class, args);
+	}
+
+}
